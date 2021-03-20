eb9c3b7f5ba7c8d1678ea49d827ea12693539d0c
deesejohn <deese.john@gmail.com>
2021-03-20 07:31:46 +0000

    Cleaning up the Lobby Component of the Lobbies SPA

--- services/lobbies-spa/src/app/lobby/lobby.component.ts
+++ services/lobbies-spa/src/app/lobby/lobby.component.ts
@@ -1,5 +1,5 @@
 import { DOCUMENT } from '@angular/common';
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, Inject, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import * as signalR from '@microsoft/signalr';
 import { HttpTransportType } from '@microsoft/signalr';
@@ -14,8 +14,9 @@ import { PlayerService } from '../player.service';
   templateUrl: './lobby.component.html',
   styleUrls: ['./lobby.component.scss']
 })
-export class LobbyComponent implements OnInit {
-  lobby$: BehaviorSubject<Lobby | undefined>;
+export class LobbyComponent implements OnInit, OnDestroy {
+  readonly connection: signalR.HubConnection;
+  readonly lobby$: BehaviorSubject<Lobby | undefined>;
 
   constructor(
     @Inject(DOCUMENT) private readonly document: Document,
@@ -23,6 +24,13 @@ export class LobbyComponent implements OnInit {
     private readonly playerService: PlayerService,
     private readonly route: ActivatedRoute,
   ) {
+    const lobbyId = this.route.snapshot.paramMap.get('id') || '';
+    this.connection = new signalR.HubConnectionBuilder()
+      .withUrl('/api/lobbies/session?lobby_id=' + lobbyId, {
+        transport: HttpTransportType.WebSockets,
+        withCredentials: true
+      })
+      .build();
     this.lobby$ = new BehaviorSubject<Lobby | undefined>(undefined);
   }
 
@@ -32,27 +40,22 @@ export class LobbyComponent implements OnInit {
         filter(lobby => !!lobby?.game_id)
       )
       .subscribe(lobby => this.redirectToGame(lobby?.game_id as string));
-
-    const lobbyId = this.route.snapshot.paramMap.get('id') || '';
-    let connection = new signalR.HubConnectionBuilder()
-      .withUrl('/api/lobbies/session?lobby_id=' + lobbyId, {
-        transport: HttpTransportType.WebSockets,
-        withCredentials: true
-      })
-      .build();
-
-    connection.on("LobbyUpdated",
+    this.connection.on("LobbyUpdated",
       (lobby: Lobby) => this.lobby$.next(lobby)
     );
+    this.connection.start();
+  }
 
-    connection.start()
-        .then(() => console.log('js connected'));
+  ngOnDestroy(): void {
+    this.connection.stop();
   }
 
   changeTeam(): void {
-    const lobbyId = this.route.snapshot.paramMap.get('id') || '';
-    const playerId = this.playerService.getPlayerId() || '';
-    this.lobbiesService.changeTeam(lobbyId, playerId).subscribe();
+    const lobbyId = this.route.snapshot.paramMap.get('id');
+    const playerId = this.playerService.getPlayerId();
+    if (lobbyId && playerId) {
+      this.lobbiesService.changeTeam(lobbyId, playerId).subscribe();
+    }
   }
 
   redirectToGame(gameId: string): void {
