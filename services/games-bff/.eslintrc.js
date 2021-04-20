2cdb66636138b873072608c776f947f59dd3db35
John Deese <deese.john@gmail.com>
2021-04-20 19:30:41 +0100

    Added .eslintrc.js to games-buff with airbnb config and resolved issues

---
+++ services/games-bff/.eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    node: true,
+  },
+  extends: [
+    'airbnb-typescript/base',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {},
+};
--- services/games-bff/index.ts
+++ services/games-bff/index.ts
@@ -1,6 +1,6 @@
-import express from 'express';
-import { json } from 'body-parser';
 import http from 'http';
+import { json } from 'body-parser';
+import express, { NextFunction, Request, Response } from 'express';
 import * as grpc from '@grpc/grpc-js';
 import { connect, StringCodec } from 'nats';
 import { Server } from 'ws';
@@ -16,13 +16,13 @@ import {
   Player as GrpcPlayer,
   SkipTurnRequest,
 } from './genproto/games_pb';
-import { Card, Game, Player } from 'models';
+import { Card, Game, Player } from './models';
 
-const app = express(),
-  server = http.createServer(app);
-const GAMES_HOST = process.env.GAMES_HOST || 'localhost:4000',
-  NATS_HOST = process.env.NATS_HOST || '',
-  PORT = 8000;
+const app = express();
+const server = http.createServer(app);
+const GAMES_HOST = process.env.GAMES_HOST || 'localhost:4000';
+const NATS_HOST = process.env.NATS_HOST || '';
+const PORT = 8000;
 
 const gameClient = new GamesServiceClient(
   GAMES_HOST,
@@ -35,6 +35,13 @@ function mapCard(dto: GrpcCard.AsObject): Card {
     label: dto.label,
     color: dto.color,
     revealed: dto.revealed,
+  };
+}
+
+function mapPlayer(dto: GrpcPlayer.AsObject): Player {
+  return {
+    player_id: dto.playerId,
+    nickname: dto.nickname,
   };
 }
 
@@ -57,104 +64,137 @@ function mapGame(dto: GrpcGame.AsObject): Game {
   };
 }
 
-function mapPlayer(dto: GrpcPlayer.AsObject): Player {
-  return {
-    player_id: dto.playerId,
-    nickname: dto.nickname,
-  };
-}
-
 app.use(json());
 
-app.get('/:game_id/', (req, res) => {
-  let request = new GetGameRequest();
+app.get('/:game_id/', (req, res, next) => {
+  const request = new GetGameRequest();
   request.setGameId(req.params.game_id);
   gameClient.getGame(request, (err, data) => {
     if (err) {
-      res.status(400).send();
-      return;
+      return next(err);
     }
-    if (!data) {
-      res.status(404).send();
-      return;
-    }
-    const response = data.getGame().toObject();
-    res.send(mapGame(response));
+    return res.send(mapGame(data.getGame().toObject()));
   });
 });
 
-app.post('/:game_id/guess', (req, res) => {
-  let request = new GuessRequest();
-  request.setGameId(req.params.game_id);
-  request.setPlayerId(req.body.player_id);
-  request.setCardId(req.body.card_id);
-  gameClient.guess(request, (err, data) => {
-    if (err) {
-      res.status(400).send();
-      return;
-    }
-    res.status(204).send();
-  });
-});
+interface GuessParams {
+  game_id: string;
+}
+interface GuessReqBody {
+  player_id: string;
+  card_id: string;
+}
+app.post(
+  '/:game_id/guess',
+  (req: Request<GuessParams, never, GuessReqBody>, res, next) => {
+    const request = new GuessRequest();
+    request.setGameId(req.params.game_id);
+    request.setPlayerId(req.body.player_id);
+    request.setCardId(req.body.card_id);
+    gameClient.guess(request, err => {
+      if (err) {
+        return next(err);
+      }
+      return res.status(204).send();
+    });
+  }
+);
 
-app.post('/:game_id/hint', (req, res) => {
-  let request = new HintRequest();
-  request.setGameId(req.params.game_id);
-  request.setPlayerId(req.body.player_id);
-  let clue = new GrpcClue();
-  clue.setNumber(req.body.number);
-  clue.setWord(req.body.word);
-  request.setClue(clue);
-  gameClient.hint(request, (err, data) => {
-    if (err) {
-      res.status(400).send();
-      return;
-    }
-    res.status(204).send();
-  });
-});
+interface HintParams {
+  game_id: string;
+}
+interface HintReqBody {
+  player_id: string;
+  number: number;
+  word: string;
+}
+app.post(
+  '/:game_id/hint',
+  (req: Request<HintParams, never, HintReqBody>, res, next) => {
+    const request = new HintRequest();
+    request.setGameId(req.params.game_id);
+    request.setPlayerId(req.body.player_id);
+    const clue = new GrpcClue();
+    clue.setNumber(req.body.number);
+    clue.setWord(req.body.word);
+    request.setClue(clue);
+    gameClient.hint(request, err => {
+      if (err) {
+        return next(err);
+      }
+      return res.status(204).send();
+    });
+  }
+);
 
-app.post('/:game_id/play_again', (req, res) => {
-  let request = new PlayAgainRequest();
-  request.setGameId(req.params.game_id);
-  request.setPlayerId(req.body.player_id);
-  gameClient.playAgain(request, (err, data) => {
-    if (err) {
-      res.status(400).send();
-      return;
-    }
-    res.status(204).send();
-  });
-});
+interface PlayAgainParams {
+  game_id: string;
+}
+interface PlayAgainReqBody {
+  player_id: string;
+  number: number;
+  word: string;
+}
+app.post(
+  '/:game_id/play_again',
+  (req: Request<PlayAgainParams, never, PlayAgainReqBody>, res, next) => {
+    const request = new PlayAgainRequest();
+    request.setGameId(req.params.game_id);
+    request.setPlayerId(req.body.player_id);
+    gameClient.playAgain(request, err => {
+      if (err) {
+        return next(err);
+      }
+      return res.status(204).send();
+    });
+  }
+);
 
-app.post('/:game_id/skip', (req, res) => {
-  let request = new SkipTurnRequest();
-  request.setGameId(req.params.game_id);
-  request.setPlayerId(req.body.player_id);
-  gameClient.skipTurn(request, (err, data) => {
-    if (err) {
-      res.status(400).send();
-      return;
-    }
-    res.status(204).send();
-  });
-});
+interface SkipParams {
+  game_id: string;
+}
+interface SkipReqBody {
+  player_id: string;
+  number: number;
+  word: string;
+}
+app.post(
+  '/:game_id/skip',
+  (req: Request<SkipParams, never, SkipReqBody>, res, next) => {
+    const request = new SkipTurnRequest();
+    request.setGameId(req.params.game_id);
+    request.setPlayerId(req.body.player_id);
+    gameClient.skipTurn(request, err => {
+      if (err) {
+        return next(err);
+      }
+      return res.status(204).send();
+    });
+  }
+);
 
 app.get('/health/live', (_, res) => res.status(204).send());
 app.get('/health/ready', (_, res) => res.status(204).send());
 
-const wss = new Server({ server: server, path: '/session' });
+app.use((err: unknown, req: Request, res: Response, next: NextFunction) => {
+  res.status(500).send({ error: 'An unknown error has occurred' });
+});
+
+const wss = new Server({ server, path: '/session' });
 wss.on('connection', (ws, req) => {
   (async () => {
     const url = new URL(req.url, 'http://localhost');
-    const game_id = url.searchParams.get('game_id').replace(/[\*\>]/, '');
+    const gameId = url.searchParams.get('game_id').replace(/[*>]/, '');
     const nc = await connect({ servers: NATS_HOST });
     const c = StringCodec();
-    const sub = nc.subscribe(`games.${game_id}`);
+    const sub = nc.subscribe(`games.${gameId}`);
+    // Correct usage of the nats.js client based on their docs
+    // https://github.com/nats-io/nats.js/
+    // eslint-disable-next-line no-restricted-syntax
     for await (const m of sub) {
       ws.send(c.decode(m.data));
     }
-  })().catch(console.log);
+  })().catch(() => {});
 });
 
-server.listen(PORT, () => console.log('[server]: Server is running'));
+server.listen(PORT);
