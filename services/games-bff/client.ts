d8d09f29ff31d029e2bfabb9f3e1755df6327afe
John Deese <deese.john@gmail.com>
2021-07-11 17:39:25 +0100

    Games BFF - Refactored out app and GameClient

---
+++ services/games-bff/client.ts
@@ -0,0 +1,142 @@
+import * as grpc from '@grpc/grpc-js';
+import { GamesServiceClient } from './genproto/games_grpc_pb';
+import {
+  Card as GrpcCard,
+  Clue as GrpcClue,
+  Game as GrpcGame,
+  GetGameRequest,
+  GuessRequest,
+  HintRequest,
+  PlayAgainRequest,
+  Player as GrpcPlayer,
+  SkipTurnRequest,
+} from './genproto/games_pb';
+import { Card, Game, Player } from './models';
+
+class GameClient {
+  private readonly gameClient: GamesServiceClient;
+
+  constructor(host: string) {
+    this.gameClient = new GamesServiceClient(
+      host,
+      grpc.credentials.createInsecure()
+    );
+  }
+
+  public get = (gameId: string): Promise<Game | null> => {
+    const request = new GetGameRequest();
+    request.setGameId(gameId);
+    return new Promise<Game | null>((resolve, reject) => {
+      this.gameClient.getGame(request, (err, data) => {
+        if (err) {
+          return reject(err);
+        }
+        const game = data?.getGame()?.toObject();
+        if (!game) {
+          return resolve(null);
+        }
+        return resolve(this.mapGame(game));
+      });
+    });
+  };
+
+  public guess = (
+    gameId: string,
+    playerId: string,
+    cardId: string
+  ): Promise<void> => {
+    const request = new GuessRequest();
+    request.setGameId(gameId);
+    request.setPlayerId(playerId);
+    request.setCardId(cardId);
+    return new Promise<void>((resolve, reject) => {
+      this.gameClient.guess(request, err => {
+        if (err) {
+          return reject(err);
+        }
+        return resolve();
+      });
+    });
+  };
+
+  public hint = (
+    gameId: string,
+    playerId: string,
+    clue: { number: number; word: string }
+  ): Promise<void> => {
+    const request = new HintRequest();
+    request.setGameId(gameId);
+    request.setPlayerId(playerId);
+    const requestClue = new GrpcClue();
+    requestClue.setNumber(clue.number);
+    requestClue.setWord(clue.word);
+    request.setClue(requestClue);
+    return new Promise<void>((resolve, reject) => {
+      this.gameClient.hint(request, err => {
+        if (err) {
+          return reject(err);
+        }
+        return resolve();
+      });
+    });
+  };
+
+  public playAgain = (gameId: string, playerId: string): Promise<void> => {
+    const request = new PlayAgainRequest();
+    request.setGameId(gameId);
+    request.setPlayerId(playerId);
+    return new Promise<void>((resolve, reject) => {
+      this.gameClient.playAgain(request, err => {
+        if (err) {
+          return reject(err);
+        }
+        return resolve();
+      });
+    });
+  };
+
+  public skipTurn = (gameId: string, playerId: string): Promise<void> => {
+    const request = new SkipTurnRequest();
+    request.setGameId(gameId);
+    request.setPlayerId(playerId);
+    return new Promise<void>((resolve, reject) => {
+      this.gameClient.skipTurn(request, err => {
+        if (err) {
+          return reject(err);
+        }
+        return resolve();
+      });
+    });
+  };
+
+  private mapCard = (dto: GrpcCard.AsObject): Card => ({
+    card_id: dto.cardId,
+    label: dto.label,
+    color: dto.color,
+    revealed: dto.revealed,
+  });
+
+  private mapPlayer = (dto: GrpcPlayer.AsObject): Player => ({
+    player_id: dto.playerId,
+    nickname: dto.nickname,
+  });
+
+  private mapGame = (dto: GrpcGame.AsObject): Game => ({
+    game_id: dto.gameId,
+    host_id: dto.hostId,
+    blue_team: dto.blueTeamList.map(this.mapPlayer),
+    blue_team_spymaster: dto.blueTeamSpymaster,
+    red_team: dto.redTeamList.map(this.mapPlayer),
+    red_team_spymaster: dto.redTeamSpymaster,
+    board: dto.boardList.map(this.mapCard),
+    key: dto.keyList.map(this.mapCard),
+    guessing: dto.guessing,
+    clue: {
+      word: String(dto.clue?.word),
+      number: Number(dto.clue?.number),
+    },
+    winner: dto.winner,
+  });
+}
+
+export default GameClient;
--- services/games-bff/index.ts
+++ services/games-bff/index.ts
@@ -1,190 +1,23 @@
 import http from 'http';
-import { json } from 'body-parser';
-import express, { NextFunction, Request, Response } from 'express';
-import * as grpc from '@grpc/grpc-js';
 import { connect, StringCodec } from 'nats';
 import { Server } from 'ws';
-import { GamesServiceClient } from './genproto/games_grpc_pb';
-import {
-  Card as GrpcCard,
-  Clue as GrpcClue,
-  Game as GrpcGame,
-  GetGameRequest,
-  GuessRequest,
-  HintRequest,
-  PlayAgainRequest,
-  Player as GrpcPlayer,
-  SkipTurnRequest,
-} from './genproto/games_pb';
-import { Card, Game, Player } from './models';
+import app from './app';
 
-const app = express();
 const server = http.createServer(app);
-const GAMES_HOST = process.env.GAMES_HOST || 'localhost:4000';
 const NATS_HOST = process.env.NATS_HOST || '';
 const PORT = 8000;
-
-const gameClient = new GamesServiceClient(
-  GAMES_HOST,
-  grpc.credentials.createInsecure()
-);
-
-function mapCard(dto: GrpcCard.AsObject): Card {
-  return {
-    card_id: dto.cardId,
-    label: dto.label,
-    color: dto.color,
-    revealed: dto.revealed,
-  };
-}
-
-function mapPlayer(dto: GrpcPlayer.AsObject): Player {
-  return {
-    player_id: dto.playerId,
-    nickname: dto.nickname,
-  };
-}
-
-function mapGame(dto: GrpcGame.AsObject): Game {
-  return {
-    game_id: dto.gameId,
-    host_id: dto.hostId,
-    blue_team: dto.blueTeamList.map(mapPlayer),
-    blue_team_spymaster: dto.blueTeamSpymaster,
-    red_team: dto.redTeamList.map(mapPlayer),
-    red_team_spymaster: dto.redTeamSpymaster,
-    board: dto.boardList.map(mapCard),
-    key: dto.keyList.map(mapCard),
-    guessing: dto.guessing,
-    clue: {
-      word: dto.clue.word,
-      number: dto.clue.number,
-    },
-    winner: dto.winner,
-  };
-}
-
-app.use(json());
-
-app.get('/:game_id/', (req, res, next) => {
-  const request = new GetGameRequest();
-  request.setGameId(req.params.game_id);
-  gameClient.getGame(request, (err, data) => {
-    if (err) {
-      return next(err);
-    }
-    return res.send(mapGame(data.getGame().toObject()));
-  });
-});
-
-interface GuessParams {
-  game_id: string;
-}
-interface GuessReqBody {
-  player_id: string;
-  card_id: string;
-}
-app.post(
-  '/:game_id/guess',
-  (req: Request<GuessParams, never, GuessReqBody>, res, next) => {
-    const request = new GuessRequest();
-    request.setGameId(req.params.game_id);
-    request.setPlayerId(req.body.player_id);
-    request.setCardId(req.body.card_id);
-    gameClient.guess(request, err => {
-      if (err) {
-        return next(err);
-      }
-      return res.status(204).send();
-    });
-  }
-);
-
-interface HintParams {
-  game_id: string;
-}
-interface HintReqBody {
-  player_id: string;
-  number: number;
-  word: string;
-}
-app.post(
-  '/:game_id/hint',
-  (req: Request<HintParams, never, HintReqBody>, res, next) => {
-    const request = new HintRequest();
-    request.setGameId(req.params.game_id);
-    request.setPlayerId(req.body.player_id);
-    const clue = new GrpcClue();
-    clue.setNumber(req.body.number);
-    clue.setWord(req.body.word);
-    request.setClue(clue);
-    gameClient.hint(request, err => {
-      if (err) {
-        return next(err);
-      }
-      return res.status(204).send();
-    });
-  }
-);
-
-interface PlayAgainParams {
-  game_id: string;
-}
-interface PlayAgainReqBody {
-  player_id: string;
-  number: number;
-  word: string;
-}
-app.post(
-  '/:game_id/play_again',
-  (req: Request<PlayAgainParams, never, PlayAgainReqBody>, res, next) => {
-    const request = new PlayAgainRequest();
-    request.setGameId(req.params.game_id);
-    request.setPlayerId(req.body.player_id);
-    gameClient.playAgain(request, err => {
-      if (err) {
-        return next(err);
-      }
-      return res.status(204).send();
-    });
-  }
-);
-
-interface SkipParams {
-  game_id: string;
-}
-interface SkipReqBody {
-  player_id: string;
-  number: number;
-  word: string;
-}
-app.post(
-  '/:game_id/skip',
-  (req: Request<SkipParams, never, SkipReqBody>, res, next) => {
-    const request = new SkipTurnRequest();
-    request.setGameId(req.params.game_id);
-    request.setPlayerId(req.body.player_id);
-    gameClient.skipTurn(request, err => {
-      if (err) {
-        return next(err);
-      }
-      return res.status(204).send();
-    });
-  }
-);
-
-app.get('/health/live', (_, res) => res.status(204).send());
-app.get('/health/ready', (_, res) => res.status(204).send());
-
-app.use((err: unknown, req: Request, res: Response, next: NextFunction) => {
-  res.status(500).send({ error: 'An unknown error has occurred' });
-});
 
 const wss = new Server({ server, path: '/session' });
 wss.on('connection', (ws, req) => {
   (async () => {
+    if (!req.url) {
+      throw new Error('Invalid URL');
+    }
     const url = new URL(req.url, 'http://localhost');
-    const gameId = url.searchParams.get('game_id').replace(/[*>]/, '');
+    const gameId = url.searchParams.get('game_id')?.replace(/[*>]/, '');
+    if (!gameId) {
+      throw new Error('Invalid game id');
+    }
     const nc = await connect({ servers: NATS_HOST });
     const c = StringCodec();
     const sub = nc.subscribe(`games.${gameId}`);
