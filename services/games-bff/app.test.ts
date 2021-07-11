f8092c712a9d06041ba18e2f7c616269197b2029
deesejohn <deese.john@gmail.com>
2021-07-11 17:46:40 +0100

    Games BFF - Refactored out app and GameClient (#71)
    * Games BFF - Refactored out app and GameClient
    
    * Removed eslint disabled

---
+++ services/games-bff/app.test.ts
@@ -0,0 +1,79 @@
+import request from 'supertest';
+import app, { gameClient } from './app';
+
+test('GET /:game_id/ Success', async () => {
+  const id = 'somefakeid';
+  const spy = jest.spyOn(gameClient, 'get').mockResolvedValue({
+    game_id: id,
+    host_id: '',
+    blue_team: [],
+    blue_team_spymaster: '',
+    red_team: [],
+    red_team_spymaster: '',
+    board: [],
+    key: [],
+    guessing: '',
+    clue: {
+      word: '',
+      number: 0,
+    },
+    winner: '',
+  });
+  await request(app).get(`/${id}/`).expect(200);
+  expect(spy).toBeCalled();
+});
+
+test('GET /:game_id/ NotFound', async () => {
+  const id = 'somefakeid';
+  const spy = jest.spyOn(gameClient, 'get').mockResolvedValue(null);
+  await request(app).get(`/${id}/`).expect(404);
+  expect(spy).toBeCalled();
+});
+
+test('POST /:game_id/guess NoContent', async () => {
+  const id = 'somefakeid';
+  const spy = jest.spyOn(gameClient, 'guess').mockResolvedValue();
+  await request(app)
+    .post(`/${id}/guess`)
+    .send({ player_id: 'ziggy-stardust', card_id: 'id' })
+    .expect(204);
+  expect(spy).toBeCalled();
+});
+
+test('POST /:game_id/hint NoContent', async () => {
+  const id = 'somefakeid';
+  const spy = jest.spyOn(gameClient, 'hint').mockResolvedValue();
+  await request(app)
+    .post(`/${id}/hint`)
+    .send({ player_id: 'ziggy-stardust', number: 1, word: 'test' })
+    .expect(204);
+  expect(spy).toBeCalled();
+});
+
+test('POST /:game_id/play_again NoContent', async () => {
+  const id = 'somefakeid';
+  const spy = jest.spyOn(gameClient, 'playAgain').mockResolvedValue();
+  await request(app)
+    .post(`/${id}/play_again`)
+    .send({ player_id: 'ziggy-stardust' })
+    .expect(204);
+  expect(spy).toBeCalled();
+});
+
+test('POST /:game_id/skip NoContent', async () => {
+  const id = 'somefakeid';
+  const spy = jest.spyOn(gameClient, 'skipTurn').mockResolvedValue();
+  await request(app)
+    .post(`/${id}/skip`)
+    .send({ player_id: 'ziggy-stardust' })
+    .expect(204);
+  expect(spy).toBeCalled();
+});
+
+test('GET /health/live NoContent', async () => {
+  await request(app).get(`/health/live`).expect(204);
+});
+
+test('GET /health/ready NoContent', async () => {
+  await request(app).get(`/health/ready`).expect(204);
+});
---
+++ services/games-bff/app.ts
@@ -0,0 +1,101 @@
+import { json } from 'body-parser';
+import express, { Request, Response } from 'express';
+import GameClient from './client';
+
+const app = express();
+const GAMES_HOST = process.env.GAMES_HOST || 'localhost:4000';
+export const gameClient = new GameClient(GAMES_HOST);
+
+app.use(json());
+
+app.get('/:game_id/', (req, res, next) => {
+  gameClient
+    .get(req.params.game_id)
+    .then(game => {
+      if (!game) {
+        res.status(404).send();
+      }
+      res.send(game);
+    })
+    .catch(err => next(err));
+});
+
+interface GuessParams {
+  game_id: string;
+}
+interface GuessReqBody {
+  player_id: string;
+  card_id: string;
+}
+app.post(
+  '/:game_id/guess',
+  (req: Request<GuessParams, never, GuessReqBody>, res, next) => {
+    gameClient
+      .guess(req.params.game_id, req.body.player_id, req.body.card_id)
+      .then(() => res.status(204).send())
+      .catch(err => next(err));
+  }
+);
+
+interface HintParams {
+  game_id: string;
+}
+interface HintReqBody {
+  player_id: string;
+  number: number;
+  word: string;
+}
+app.post(
+  '/:game_id/hint',
+  (req: Request<HintParams, never, HintReqBody>, res, next) => {
+    gameClient
+      .hint(req.params.game_id, req.body.player_id, req.body)
+      .then(() => res.status(204).send())
+      .catch(err => next(err));
+  }
+);
+
+interface PlayAgainParams {
+  game_id: string;
+}
+interface PlayAgainReqBody {
+  player_id: string;
+  number: number;
+  word: string;
+}
+app.post(
+  '/:game_id/play_again',
+  (req: Request<PlayAgainParams, never, PlayAgainReqBody>, res, next) => {
+    gameClient
+      .playAgain(req.params.game_id, req.body.player_id)
+      .then(() => res.status(204).send())
+      .catch(err => next(err));
+  }
+);
+
+interface SkipParams {
+  game_id: string;
+}
+interface SkipReqBody {
+  player_id: string;
+  number: number;
+  word: string;
+}
+app.post(
+  '/:game_id/skip',
+  (req: Request<SkipParams, never, SkipReqBody>, res, next) => {
+    gameClient
+      .skipTurn(req.params.game_id, req.body.player_id)
+      .then(() => res.status(204).send())
+      .catch(err => next(err));
+  }
+);
+
+app.get('/health/live', (_, res) => res.status(204).send());
+app.get('/health/ready', (_, res) => res.status(204).send());
+
+app.use((err: unknown, req: Request, res: Response) => {
+  res.status(500).send({ error: 'An unknown error has occurred' });
+});
+
+export default app;
