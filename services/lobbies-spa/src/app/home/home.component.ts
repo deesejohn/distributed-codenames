6cd42afa6a5d0219483bfbc573cd2bd288094fca
John Deese <deese.john@gmail.com>
2022-07-15 19:38:04 +0100

    Upgraded Lobbies SPA to Angular 14 (#99)
    * Upgraded Lobbies SPA to Angular 14

--- services/lobbies-spa/src/app/home/home.component.ts
+++ services/lobbies-spa/src/app/home/home.component.ts
@@ -1,25 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { LobbiesService } from '../lobbies.service';
+import { LobbiesService } from '../core/lobbies.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-
   constructor(
     private readonly lobbiesService: LobbiesService,
     private readonly router: Router
-  ) { }
+  ) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   createLobby(): void {
-    this.lobbiesService.createLobby().subscribe(
-      (lobbyId) => this.router.navigateByUrl('/' + lobbyId)
-    );
+    this.lobbiesService
+      .createLobby()
+      .subscribe(lobbyId => this.router.navigateByUrl('/' + lobbyId));
   }
 }
