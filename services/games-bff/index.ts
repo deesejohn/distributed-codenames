c376c513f963487190f9a98f173e79cd9bce08a7
John Deese <deese.john@gmail.com>
2022-07-23 12:10:43 +0100

    Games BFF sockets registration moved to src/sockets.ts

--- services/games-bff/index.ts
+++ services/games-bff/index.ts
@@ -1,43 +1,17 @@
 import http from 'http';
 import { connect } from 'nats';
-import { URL } from 'url';
 import { Server } from 'ws';
 import app from './src/app';
-import GameClient from './src/client';
-import { Game } from './genproto/games_pb';
+import register from './src/sockets';
 
-const server = http.createServer(app);
 const NATS_HOST = process.env.NATS_HOST || 'localhost';
 const PORT = 8000;
 
-const bootstrap = async () => {
-  const nc = await connect({ servers: NATS_HOST });
-  const wss = new Server({ server, path: '/session' });
-  wss.on('connection', (ws, req) => {
-    (async () => {
-      if (!req.url) {
-        throw new Error('Bad request format: invalid url');
-      }
-      const url = new URL(req.url, 'http://localhost');
-      const gameId = url.searchParams.get('game_id')?.replace(/[*>]/, '');
-      if (!gameId) {
-        throw new Error('Missing required query parameter: game_id');
-      }
-      const sub = nc.subscribe(`games.${gameId}`);
-      // Correct usage of the nats.js client based on their docs
-      // https://github.com/nats-io/nats.js/
-      // eslint-disable-next-line no-restricted-syntax
-      for await (const m of sub) {
-        const game = Game.deserializeBinary(m.data).toObject();
-        ws.send(JSON.stringify(GameClient.mapGame(game)));
-      }
-      // eslint-disable-next-line no-console
-    })().catch((error: unknown) => console.log(error));
-  });
-};
-
 (async () => {
-  await bootstrap();
+  const nc = await connect({ servers: NATS_HOST });
+  const server = http.createServer(app);
+  const wss = new Server({ server, path: '/session' });
+  register(wss, nc);
   server.listen(PORT);
   // eslint-disable-next-line no-console
-})().catch((error: unknown) => console.log(error));
+})().catch(error => console.log(error));
