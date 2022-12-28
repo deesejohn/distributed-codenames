2ac6161da8d199ba17c20732ffb7788c954f3a91
John Deese <deese.john@gmail.com>
2022-12-28 09:53:20 +0000

    lobbies-spa angular v14 -> v15 and replaced flex-layout with tailwindcss (#110)
    Signed-off-by: deesejohn <deese.john@gmail.com>

--- services/lobbies-spa/src/app/app.component.ts
+++ services/lobbies-spa/src/app/app.component.ts
@@ -1,20 +1,15 @@
 import { DOCUMENT } from '@angular/common';
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   public readonly playersUrl: string;
 
-  constructor(
-    @Inject(DOCUMENT) private readonly document: Document,
-  ) {
+  constructor(@Inject(DOCUMENT) private readonly document: Document) {
     this.playersUrl = `/players?redirect_uri=${this.document.location.pathname}`;
-  }
-
-  ngOnInit(): void {
   }
 }
