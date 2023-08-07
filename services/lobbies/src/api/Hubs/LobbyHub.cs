0e5d33628cda7906b311a3eb27f24fc8df217c1d
John Deese <deese.john@gmail.com>
2023-08-07 10:28:15 +0100

    chore(lobbies,devcont): update dotnet and add devcontainer exts (#113)
    Signed-off-by: deesejohn <deese.john@gmail.com>

--- services/lobbies/src/api/Hubs/LobbyHub.cs
+++ services/lobbies/src/api/Hubs/LobbyHub.cs
@@ -22,12 +22,27 @@ namespace lobbies.api.Hubs
         {
             await base.OnConnectedAsync();
             var lobbyId = GetLobbyId();
+            if (lobbyId == null)
+            {
+                await base.Clients.Caller.SendAsync("Error: invalid lobby id");
+                return;
+            }
             var playerId = GetUserId();
+            if (playerId == null)
+            {
+                await base.Clients.Caller.SendAsync("Error: invalid palyer id");
+                return;
+            }
             await Groups.AddToGroupAsync(
                 Context.ConnectionId,
                 lobbyId
             );
             var player = await _playerService.GetAsync(playerId);
+            if (player == null)
+            {
+                await base.Clients.Caller.SendAsync("Error: invalid palyer id");
+                return;
+            }
             await _lobbyService.AddPlayerAsync(lobbyId, player);
             await Clients.Caller.SendAsync(
                 LOBBY_UPDATED,
@@ -35,21 +50,32 @@ namespace lobbies.api.Hubs
             );
         }
 
-        public override async Task OnDisconnectedAsync(Exception exception)
+        public override async Task OnDisconnectedAsync(Exception? exception)
         {
             var lobbyId = GetLobbyId();
+            if (lobbyId == null)
+            {
+                await base.Clients.Caller.SendAsync("Error: invalid lobby id");
+                return;
+            }
+            var playerId = GetUserId();
+            if (playerId == null)
+            {
+                await base.Clients.Caller.SendAsync("Error: invalid palyer id");
+                return;
+            }
             await Groups.RemoveFromGroupAsync(
                 Context.ConnectionId,
                 lobbyId
             );
-            await _lobbyService.RemovePlayerAsync(lobbyId, GetUserId());
+            await _lobbyService.RemovePlayerAsync(lobbyId, playerId);
             await base.OnDisconnectedAsync(exception);
         }
 
-        private string GetLobbyId()
-            => Context.GetHttpContext().Request.Query["lobby_id"].ToString();
+        private string? GetLobbyId()
+            => Context.GetHttpContext()?.Request.Query["lobby_id"].ToString();
 
-        private string GetUserId()
-            => Context.GetHttpContext().Request.Cookies["player_id"].ToString();
+        private string? GetUserId()
+            => Context.GetHttpContext()?.Request.Cookies["player_id"]?.ToString();
     }
 }
